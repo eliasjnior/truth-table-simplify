bb546364c33b81a0ea8e8fdbce1089e9fdea0329
Elias Júnior <me@eliasjr.dev>
2020-10-09 14:44:41 -0300

    Updates

---
+++ precache-manifest.2af9afc3a00acdd8b3bba48ce67323c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdaeae2ba6727ba63b05be9523dd7485",
+    "url": "/truth-table-simplify/index.html"
+  },
+  {
+    "revision": "0cd76e0759b3581caf74",
+    "url": "/truth-table-simplify/static/css/main.12d3dcfc.chunk.css"
+  },
+  {
+    "revision": "7001a172fd8f2f7dbcc0",
+    "url": "/truth-table-simplify/static/js/2.1d8f8cdc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/truth-table-simplify/static/js/2.1d8f8cdc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cd76e0759b3581caf74",
+    "url": "/truth-table-simplify/static/js/main.b0fc5629.chunk.js"
+  },
+  {
+    "revision": "366b7e21d6b57b82bf07",
+    "url": "/truth-table-simplify/static/js/runtime-main.ac7fc205.js"
+  }
+]);
